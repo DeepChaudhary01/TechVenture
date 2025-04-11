363fcb213986b6edbef82f4472e74ce0306f172e
pu9052473 <pu9052473@gmail.com>
2025-04-11 21:20:51 +0530

    Initial landing page

--- src/app/dashboard/[role]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "@/components/dashboard/Dashboard";
-import { useRouter } from "next/router";
-
-export default function DashboardPage() {
-  const router = useRouter();
-  const { role } = router.query;
-
-  return <Dashboard role={role as string} />;
-}
